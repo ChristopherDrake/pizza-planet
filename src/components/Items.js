53f49c0050743b523cd828173d3b97403fcfb26b
christopher.massonet <christopher.massonet@smoothstack.com>
2023-04-24 16:46:36 -0600

    Added menu pictures.
    Added counter to menu items.

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,37 +4,43 @@ const Items = {
             id: '1',
             name: 'Pepperoni Pizza',
             price: 1299,
-            description: 'Cheese, Pepperoni.'
+            description: 'Cheese, Pepperoni.',
+            image: '../images/pepperoni.jpg'
         },
         {
             id: '2',
             name: 'Margherrita Pizza',
             price: 1099,
-            description: 'Cheese, Basil.'
+            description: 'Cheese, Basil.',
+            image: '../images/margherita.jpg'
         },
         {
             id: '3',
             name: 'Hawaiian Pizza',
             price: 1399,
-            description: 'Cheese, Ham, Pineapple.'
+            description: 'Cheese, Ham, Pineapple.',
+            image: '../images/hawaiian.jpg'
         },
         {
             id: '4',
             name: 'Mozzarella Sticks',
             price: 599,
-            description: 'Mozzarella cheese with Marinara dip.'
+            description: 'Mozzarella cheese with Marinara dip.',
+            image: '../images/mozzarella.jpg'
         },
         {
             id: '5',
             name: 'Wings',
             price: 1099,
-            description: 'Traditional and Boneless Wings.'
+            description: 'Traditional and Boneless Wings.',
+            image: '../images/wings.jpg'
         },
         {
             id: '6',
             name: 'Soft Drinks',
             price: 399,
-            description: 'Coke, Sprite, Fanta, Dr. Pepper'
+            description: 'Coke, Sprite, Fanta, Dr. Pepper',
+            image: '../images/soda.jpg'
         },
     ]
 }
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import pepperoniImage from '../images/pepperoni.jpg';
+import margheritaImage from '../images/margherita.jpg';
+import hawaiianImage from '../images/hawaiian.jpg';
+import mozzarellaImage from '../images/mozzarella.jpg';
+import wingsImage from '../images/wings.jpg';
+import sodaImage from '../images/soda.jpg';
 
 function MenuItem(props) {
   const { product, handleAddToCart } = props;
@@ -7,9 +13,29 @@ function MenuItem(props) {
     handleAddToCart(product);
   };
 
+  const getImage = () => {
+    switch (product.image) {
+      case '../images/pepperoni.jpg':
+        return pepperoniImage;
+      case '../images/margherita.jpg':
+        return margheritaImage;
+      case '../images/hawaiian.jpg':
+        return hawaiianImage;
+      case '../images/mozzarella.jpg':
+        return mozzarellaImage;
+      case '../images/wings.jpg':
+        return wingsImage;
+      case '../images/soda.jpg':
+        return sodaImage;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="menu-item">
       <h3>{product.name}</h3>
+      <img src={getImage()} alt={product.name} width="100" height="100" />
       <p>{product.description}</p>
       <p>${(product.price / 100).toFixed(2)}</p>
       <button onClick={handleClick}>Add to Cart</button>
