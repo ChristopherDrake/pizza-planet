bc5bf81701a844e90340b309a857fd0c084e425c
christopher.massonet <christopher.massonet@smoothstack.com>
2023-04-30 17:20:52 -0600

    Stripe fix

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,28 +1,19 @@
 module.exports = {
   env: {
-    es6: true,
+    es2021: true,
     node: true,
   },
-  parserOptions: {
-    "ecmaVersion": 2018,
-  },
   extends: [
-    "eslint:recommended",
-    "google",
+    'eslint:recommended',
+    'google',
   ],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+  parserOptions: {
+    ecmaVersion: 12,
   },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
-    },
-  ],
-  globals: {},
+  rules: {
+    'quotes': ['error', 'single'],
+    'max-len': ['error', { 'code': 80 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'linebreak-style': ['error', 'unix'],
+  },
 };
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,26 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const Stripe = require("stripe");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const Stripe = require('stripe');
+const cors = require('cors');
+const express = require('express');
 
 admin.initializeApp();
-const stripe = new Stripe("sk_test_51N2KiYDpQVHsLbUuRCYgVGWWPEmztAp8H" +
-"Ri1gaxsU9iOS3b3t36HPNcQUOrc0R2S44hRtpNVicslxrkh7blhLlxo00CS9vVlak");
+const stripe = new Stripe('sk_test_51N2KiYDpQVHsLbUuRCYgVGWWPEmztAp8H' +
+  'Ri1gaxsU9iOS3b3t36HPNcQUOrc0R2S44hRtpNVicslxrkh7blhLlxo00CS9vVlak');
 
-exports.createPaymentIntent = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).send("Method Not Allowed");
+const app = express();
+
+const corsOptions = {
+  origin: 'https://christopherdrake.github.io',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.post('/createPaymentIntent', async (req, res) => {
+  if (req.method !== 'POST') {
+    res.status(405).send('Method Not Allowed');
     return;
   }
 
@@ -16,12 +28,14 @@ exports.createPaymentIntent = functions.https.onRequest(async (req, res) => {
     const {amount} = req.body;
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
-      currency: "usd",
+      currency: 'usd',
     });
 
     res.json({clientSecret: paymentIntent.client_secret});
   } catch (err) {
     console.error(err);
-    res.status(500).json({error: "Failed to create payment intent"});
+    res.status(500).json({error: 'Failed to create payment intent'});
   }
 });
+
+exports.createPaymentIntent = functions.https.onRequest(app);
